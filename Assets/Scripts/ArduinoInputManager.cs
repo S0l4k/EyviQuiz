c2dd5e9023278c0b5d1fc5422ba8faf3d02647ae
S0l4k <lukasz.drobik6@gmail.com>
2025-08-25 02:34:06 +0200

    dziala?

---
+++ Assets/Scripts/ArduinoInputManager.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.IO.Ports;
+using UnityEngine.UI;
+
+public class ArduinoInputManager : MonoBehaviour
+{
+    [Header("Arduino Settings")]
+    public string portName = "COM3";
+    public int baudRate = 9600;
+
+    [Header("UI Host Controls")]
+    public GameObject correctAnswerButton;
+    public GameObject wrongAnswerButton;
+
+    [Header("Host Next Buttons")]
+    public GameObject nextQuestionButton;
+    public GameObject nextRoundButton;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip[] playerBuzzSounds; 
+    public AudioClip correctAnswerSound;
+    public AudioClip wrongAnswerSound;
+
+    private SerialPort serialPort;
+    private bool playerBuzzed = false;
+    public bool canBuzz = false;
+    private int currentPlayer = -1;
+
+    public QuizSetup quizSetup;
+
+    void Start()
+    {
+        correctAnswerButton.SetActive(false);
+        wrongAnswerButton.SetActive(false);
+
+        serialPort = new SerialPort(portName, baudRate);
+        try
+        {
+            serialPort.Open();
+            serialPort.ReadTimeout = 50;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Błąd otwierania portu: " + e.Message);
+        }
+    }
+
+    void Update()
+    {
+        if (!playerBuzzed && canBuzz)
+        {
+            // --- klawiatura do testów ---
+            if (Input.GetKeyDown(KeyCode.Alpha1)) SimulateBuzz(1);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) SimulateBuzz(2);
+            if (Input.GetKeyDown(KeyCode.Alpha3)) SimulateBuzz(3);
+            if (Input.GetKeyDown(KeyCode.Alpha4)) SimulateBuzz(4);
+
+            // --- Arduino ---
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                try
+                {
+                    string data = serialPort.ReadLine().Trim();
+                    if (!string.IsNullOrEmpty(data))
+                    {
+                        int playerNumber;
+                        if (int.TryParse(data, out playerNumber))
+                        {
+                            PlayerBuzz(playerNumber);
+                        }
+                    }
+                }
+                catch (System.TimeoutException) { }
+            }
+        }
+    }
+
+    public void OnCorrectAnswer()
+    {
+        Debug.Log("Prowadzący: dobra odpowiedź gracza " + (currentPlayer + 1));
+        if (currentPlayer >= 0 && quizSetup != null)
+        {
+            quizSetup.AddPoint(currentPlayer);
+        }
+
+       
+        PlaySound(correctAnswerSound);
+
+        ResetBuzz();
+    }
+
+    public void OnWrongAnswer()
+    {
+        Debug.Log("Prowadzący: zła odpowiedź gracza " + (currentPlayer + 1));
+
+        
+        PlaySound(wrongAnswerSound);
+
+        ResetBuzz();
+    }
+
+    private void ResetBuzz()
+    {
+        correctAnswerButton.SetActive(false);
+        wrongAnswerButton.SetActive(false);
+        playerBuzzed = false;
+        currentPlayer = -1;
+        EnableNextButtons();
+    }
+
+    void OnApplicationQuit()
+    {
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            serialPort.Close();
+        }
+    }
+
+    void PlayerBuzz(int playerNumber)
+    {
+        int playerIndex = playerNumber - 1;
+        if (playerIndex < 0 || playerIndex >= quizSetup.playerScores.Length) return;
+
+        if (quizSetup.playerScores[playerIndex] < 0)
+        {
+            Debug.Log("Gracz " + playerNumber + " jest wyeliminowany i nie może się zgłaszać!");
+            return;
+        }
+
+        currentPlayer = playerIndex;
+        Debug.Log("Gracz zgłosił się: " + playerNumber);
+
+       
+        if (playerIndex < playerBuzzSounds.Length)
+            PlaySound(playerBuzzSounds[playerIndex]);
+
+        correctAnswerButton.SetActive(true);
+        wrongAnswerButton.SetActive(true);
+        playerBuzzed = true;
+        canBuzz = false;
+    }
+
+    void SimulateBuzz(int playerNumber)
+    {
+        PlayerBuzz(playerNumber);
+    }
+
+    public void OnNextQuestion()
+    {
+        Debug.Log("Prowadzący: następne pytanie – gracze mogą się zgłaszać");
+        canBuzz = true;
+        playerBuzzed = false;
+        nextQuestionButton.SetActive(false);
+        nextRoundButton.SetActive(false);
+    }
+
+    public void OnNextRound()
+    {
+        Debug.Log("Prowadzący: następna runda");
+        quizSetup.NextRound();
+        canBuzz = false;
+        nextQuestionButton.SetActive(false);
+        nextRoundButton.SetActive(false);
+    }
+
+    public void EnableNextButtons()
+    {
+        nextQuestionButton.SetActive(true);
+        nextRoundButton.SetActive(true);
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (audioSource != null && clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
+
---
+++ Assets/Scripts/QuizSetup.cs
@@ -0,0 +1,279 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class QuizSetup : MonoBehaviour
+{
+    private int currentRound = 1;
+    private int totalRounds = 3; 
+
+    [Header("UI Elements")]
+    public Button startQuizButton;
+    public GameObject playerNamesPanel;
+    public TMP_InputField[] playerNameInputs;
+    public Button nextButton;
+
+    [Header("Start Backgrounds (Initial UI Panels)")]
+    public GameObject playersBackground;
+    public GameObject leaderBackground;
+
+    [Header("Round Intros (Players + Leader)")]
+    public GameObject playersRound1Background;
+    public GameObject leaderRound1Background;
+    public GameObject playersRound2Background;
+    public GameObject leaderRound2Background;
+    public GameObject playersRound3Background;
+    public GameObject leaderRound3Background;
+
+    [Header("Game Backgrounds (After Intro)")]
+    public GameObject playersGameBackground;
+    public GameObject leaderGameBackground;
+
+    [Header("Player Name Displays")]
+    public TMP_Text[] playerNameTextsHost;
+    public TMP_Text[] playerNameTextsPlayers;
+
+    [Header("Scores")]
+    public TMP_Text[] playerScoreTextsHost;
+    public TMP_Text[] playerScoreTextsPlayers;
+
+    [Header("Ending")]
+    public GameObject playersFinalScreen;
+    public GameObject leaderFinalScreen;
+    public TMP_Text finalWinnerText;
+
+    private string[] playerNames = new string[4];
+    public int[] playerScores = new int[4];
+
+    [Header("References")]
+    public ArduinoInputManager arduinoInputManager;
+
+    void Start()
+    {
+        playerNamesPanel.SetActive(false);
+        nextButton.gameObject.SetActive(false);
+
+        startQuizButton.onClick.AddListener(OnStartQuiz);
+        nextButton.onClick.AddListener(OnNext);
+
+        foreach (var input in playerNameInputs)
+        {
+            input.onValueChanged.AddListener(delegate { CheckPlayerNames(); });
+        }
+        for (int i = 0; i < playerScores.Length; i++)
+        {
+            playerScores[i] = 0;
+        }
+        UpdateScoreUI();
+
+        playersRound1Background.SetActive(false);
+        leaderRound1Background.SetActive(false);
+        playersRound2Background.SetActive(false);
+        leaderRound2Background.SetActive(false);
+        playersRound3Background.SetActive(false);
+        leaderRound3Background.SetActive(false);
+        playersGameBackground.SetActive(false);
+        leaderGameBackground.SetActive(false);
+        playersFinalScreen.SetActive(false);
+        leaderFinalScreen.SetActive(false);
+    }
+
+    void OnStartQuiz()
+    {
+        startQuizButton.gameObject.SetActive(false);
+        playerNamesPanel.SetActive(true);
+    }
+
+    void CheckPlayerNames()
+    {
+        bool allFilled = true;
+        for (int i = 0; i < playerNameInputs.Length; i++)
+        {
+            if (string.IsNullOrWhiteSpace(playerNameInputs[i].text))
+            {
+                allFilled = false;
+                break;
+            }
+        }
+        nextButton.gameObject.SetActive(allFilled);
+    }
+
+    void OnNext()
+    {
+        for (int i = 0; i < playerNameInputs.Length; i++)
+        {
+            playerNames[i] = playerNameInputs[i].text.Trim();
+        }
+
+        playerNamesPanel.SetActive(false);
+        playersBackground.SetActive(false);
+        leaderBackground.SetActive(false);
+
+        for (int i = 0; i < playerNameTextsHost.Length; i++)
+            playerNameTextsHost[i].text = playerNames[i];
+
+        for (int i = 0; i < playerNameTextsPlayers.Length; i++)
+            playerNameTextsPlayers[i].text = playerNames[i];
+
+        Debug.Log("Gracze: " + string.Join(", ", playerNames));
+
+        StartCoroutine(ShowRoundIntro(playersRound1Background, leaderRound1Background, 3f, playersGameBackground, leaderGameBackground));
+    }
+
+    IEnumerator ShowRoundIntro(GameObject playersIntro, GameObject leaderIntro, float delay, GameObject playersFinal, GameObject leaderFinal)
+    {
+        playersIntro.SetActive(true);
+        leaderIntro.SetActive(true);
+
+        yield return new WaitForSeconds(delay);
+
+        playersIntro.SetActive(false);
+        leaderIntro.SetActive(false);
+
+        playersFinal.SetActive(true);
+        leaderFinal.SetActive(true);
+
+        if (arduinoInputManager != null)
+        {
+            arduinoInputManager.EnableNextButtons();
+        }
+    }
+
+    public void AddPoint(int playerIndex)
+    {
+        if (playerIndex >= 0 && playerIndex < playerScores.Length)
+        {
+            playerScores[playerIndex]++;
+            UpdateScoreUI();
+        }
+    }
+
+    void UpdateScoreUI()
+    {
+        for (int i = 0; i < playerScores.Length; i++)
+        {
+            playerScoreTextsHost[i].text = playerScores[i].ToString();
+            playerScoreTextsPlayers[i].text = playerScores[i].ToString();
+        }
+    }
+
+    public void NextRound()
+    {
+       
+        EliminateLowestScorePlayer();
+
+        
+        for (int i = 0; i < playerScores.Length; i++)
+        {
+            if (playerScores[i] >= 0)
+                playerScores[i] = 0;
+        }
+        UpdateScoreUI();
+
+        currentRound++;
+
+        playersRound1Background.SetActive(false);
+        leaderRound1Background.SetActive(false);
+        playersRound2Background.SetActive(false);
+        leaderRound2Background.SetActive(false);
+        playersRound3Background.SetActive(false);
+        leaderRound3Background.SetActive(false);
+
+        playersGameBackground.SetActive(false);
+        leaderGameBackground.SetActive(false);
+
+        if (currentRound > totalRounds)
+        {
+            EndGame();
+        }
+        else
+        {
+            switch (currentRound)
+            {
+                case 2:
+                    StartCoroutine(ShowRoundIntro(playersRound2Background, leaderRound2Background, 4f, playersGameBackground, leaderGameBackground));
+                    break;
+                case 3:
+                    StartCoroutine(ShowRoundIntro(playersRound3Background, leaderRound3Background, 4f, playersGameBackground, leaderGameBackground));
+                    break;
+            }
+        }
+    }
+
+    private void EliminateLowestScorePlayer()
+    {
+        int minScore = int.MaxValue;
+        int playerToEliminate = -1;
+
+        for (int i = 0; i < playerScores.Length; i++)
+        {
+            if (playerScores[i] >= 0 && playerScores[i] < minScore)
+            {
+                minScore = playerScores[i];
+                playerToEliminate = i;
+            }
+        }
+
+        if (playerToEliminate != -1)
+        {
+            Debug.Log("Eliminowany gracz: " + playerNames[playerToEliminate]);
+            playerScores[playerToEliminate] = -1;
+
+            playerScoreTextsHost[playerToEliminate].gameObject.SetActive(false);
+            playerScoreTextsPlayers[playerToEliminate].gameObject.SetActive(false);
+            playerNameTextsHost[playerToEliminate].gameObject.SetActive(false);
+            playerNameTextsPlayers[playerToEliminate].gameObject.SetActive(false);
+        }
+    }
+
+    private void EndGame()
+    {
+        playersGameBackground.SetActive(false);
+        leaderGameBackground.SetActive(false);
+
+        playersFinalScreen.SetActive(true);
+        leaderFinalScreen.SetActive(true);
+
+        int winnerIndex = -1;
+        int highestScore = -1;
+        for (int i = 0; i < playerScores.Length; i++)
+        {
+            if (playerScores[i] > highestScore)
+            {
+                highestScore = playerScores[i];
+                winnerIndex = i;
+            }
+        }
+
+        if (winnerIndex != -1)
+            finalWinnerText.text = playerNames[winnerIndex] + " - Congratulations!";
+        else
+            finalWinnerText.text = "No winner!";
+    }
+
+    public void ResetQuiz()
+    {
+        
+        for (int i = 0; i < playerScores.Length; i++)
+        {
+            playerScores[i] = 0;
+
+            playerScoreTextsHost[i].gameObject.SetActive(true);
+            playerScoreTextsPlayers[i].gameObject.SetActive(true);
+            playerNameTextsHost[i].gameObject.SetActive(true);
+            playerNameTextsPlayers[i].gameObject.SetActive(true);
+        }
+
+        UpdateScoreUI();
+        currentRound = 1;
+
+        playersFinalScreen.SetActive(false);
+        leaderFinalScreen.SetActive(false);
+
+        playersBackground.SetActive(true);
+        leaderBackground.SetActive(true);
+
+        startQuizButton.gameObject.SetActive(true);
+    }
+}
